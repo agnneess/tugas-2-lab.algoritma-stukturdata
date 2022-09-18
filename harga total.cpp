d2bc7e50d81b89c73e2a55f6f9463581056d2b9d
agnneess <113438606+agnneess@users.noreply.github.com>
2022-09-18 21:28:41 +0700

    Add files via upload

---
+++ harga total.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int main()
+{
+float jumlah, harga, total ;
+  cout <<"Masukan jumlah barang = ";
+  cin >>jumlah;
+  cout <<"Harga barang = Rp. ";
+  cin >>harga;
+  cout <<"total yang harus dibayar = Rp.13500";
+  cin >>total;
+return 0; 
+
+}
